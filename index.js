b37941b184c0484e5cb930f54752f0d76c919a33
Juan Pablo <106168706+Jpzinn654@users.noreply.github.com>
2023-04-14 11:06:07 -0300

    Refatoração de códigos

--- index.js
+++ index.js
@@ -34,6 +34,48 @@ app.get('/listarPerfume', (req, res)=>{
     });
 });
 
+app.get('/editarPerfume/:cod_perfume', (req, res)=>{
+
+    let {cod_perfume} = req.params
+    
+    urlListarPerfumePK = `http://localhost:3000/listarPerfumePK/${cod_perfume}`
+
+    axios.get(urlListarPerfumePK)
+    .then((response)=>{
+
+        // console.log(response.data);
+
+        let perfume = response.data;
+        res.render('perfumes/editarPerfume', {perfume});
+    })
+    
+})
+
+app.post('/editarPerfume', (req, res)=>{
+
+    let urlEditar = 'http://localhost:3000/alterarPerfume'
+
+    axios.put(urlEditar, req.body)
+    .then((response)=>{
+        res.redirect('/listarPerfume')
+    })
+});
+
+
+app.get('/deletarPerfume/:cod_perfume', (req, res)=>{
+    console.log(req.params);
+ 
+     let {cod_perfume} = req.params;
+ 
+     const urlExcluirPerfume = `http://localhost:3000/deletarPerfume/${cod_perfume}`;
+ 
+     axios.delete(urlExcluirPerfume)
+     .then((response)=>{
+         res.redirect('/listarPerfume');
+     });
+     
+ });
+
 app.listen(3001, ()=>{
     console.log("SERVIDOR FRONTEND RODANDO EM - http://localhost:3001");
 });
